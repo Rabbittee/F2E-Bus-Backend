6fb33ed9ce93819202ec2dc42ad9f1904d0ce469
HuskyHsu <schi.hsu@gmail.com>
2021-10-20 23:09:39 +0800

    feat: 🎸 add queries api query parameters and validations

--- app/api/routes/queries.py
+++ app/api/routes/queries.py
@@ -1,12 +1,16 @@
+from typing import Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
-router = APIRouter(
-    prefix="/items",
-    tags=["query"]
-)
+router = APIRouter(prefix="/queries", tags=["query"])
 
 
-@router.get("/")
-async def query(item_id: str):
-    ...
+@router.get("/recommend")
+async def query(q: str,
+                location: Optional[str] = Query(
+                    None,
+                    min_length=6,
+                    max_length=22,
+                    regex="^\d{2}.?\d{0,7},\d{3}.?\d{0,7}$")):
+
+    return {"location": location, "q": q}
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .api.routes import queries
+from api.routes import queries
 
 
 app = FastAPI()
