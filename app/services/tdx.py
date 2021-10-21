409436acd43d1fab4171513f5f0d06bddb42898b
kayac-chang <egg734631@gmail.com>
2021-10-21 14:34:34 +0800

    feat: 🎸 add tdx service auth

---
+++ app/services/tdx.py
@@ -0,0 +1,52 @@
+import requests
+import time
+from wsgiref.handlers import format_date_time
+from hmac import digest
+from hashlib import sha1
+from base64 import b64encode
+
+from config import settings
+
+HOST = settings.TDX_HOST
+API_ID = settings.TDX_API_ID
+API_KEY = settings.TDX_API_KEY
+
+
+def signature(date: str, key: str):
+    return b64encode(
+        digest(
+            key=bytes(key, "utf-8"),
+            msg=bytes(f'x-date: {date}', "utf-8"),
+            digest=sha1
+        )
+    ).decode()
+
+
+def hmac(username: str, signature: str):
+    return "hmac " + ",".join(
+        list(
+            map(
+                lambda x: f'{x[0]}="{x[1]}"',
+                {
+                    'username': username,
+                    'algorithm': 'hmac-sha1',
+                    'headers': 'x-date',
+                    'signature': signature
+                }.items()
+            )
+        )
+    )
+
+
+def GET(url: str):
+    current_time = format_date_time(time.time())
+
+    headers = {
+        'Authorization': hmac(
+            username=API_ID,
+            signature=signature(current_time, API_KEY)
+        ),
+        'x-date': current_time,
+    }
+
+    return requests.get(HOST + url, headers=headers)
