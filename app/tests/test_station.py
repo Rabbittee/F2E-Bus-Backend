dbb4385a80a452fcb918b505ce0562daa1cb7763
kayac-chang <egg734631@gmail.com>
2021-11-01 21:11:05 +0800

    fix: 🐛 fix search by name

--- app/tests/test_station.py
+++ app/tests/test_station.py
@@ -1,5 +1,5 @@
 from app.services.tdx import get_stations_in
-from app.models.Station.views import add, select_by_id
+from app.models.Station.views import add, select_by_id, is_exist, search_by_name
 from app.models.Constant import City
 
 from unittest import IsolatedAsyncioTestCase
@@ -17,7 +17,27 @@ class TestStation(IsolatedAsyncioTestCase):
 
         await add(*stations)
 
+    async def test_is_exist_with_exist_id(self):
+        r = await is_exist(id="TPE10")
+
+        self.assertIsNotNone(r)
+
     async def test_select_by_id_with_exist_id(self):
         r = await select_by_id("TPE10")
 
         self.assertIsNotNone(r)
+
+    async def test_search_by_name_with_exist_name(self):
+        r = await search_by_name("八勢里")
+
+        self.assertTrue(r)
+
+    async def test_search_by_name_with_glob(self):
+        r = await search_by_name("八勢*")
+
+        self.assertTrue(r)
+
+    async def test_search_by_name_with_empty_str(self):
+        r = await search_by_name("")
+
+        self.assertFalse(r)
